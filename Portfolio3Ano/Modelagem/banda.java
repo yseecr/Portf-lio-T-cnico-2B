693eae8c109274507325b354022f5717f1c451a3
yseecr <83797459+yseecr@users.noreply.github.com>
2022-04-29 10:27:08 -0300

    Create banda.java

---
+++ Portfolio3Ano/Modelagem/banda.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Banda{
+  private ArrayList<String> integrantes;
+  private float cache;
+  private String contato;
+  private String carga_horaria;
+
+public Banda(float cache, String contato, String carga_horaria){
+  this.cache = cache;
+  this.contato = contato;
+  this.carga_horaria = carga_horaria;
+}
+  public void procurar_bares(String contato){
+  }
+  public boolean arranjar_instrumento(){
+    return true;
+  }
+}
