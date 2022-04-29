6173fa3460da31ddaa6cda7837e7d6069c04ae69
yseecr <83797459+yseecr@users.noreply.github.com>
2022-04-29 10:27:28 -0300

    Create genero.java

---
+++ Portfolio3Ano/Modelagem/genero.java
@@ -0,0 +1,19 @@
+public class Genero{
+  private String rock;
+  private String sertanejo;
+  private String pagode;
+  private String samba;
+  private String bossa_nova;
+  private String pop;
+  private String indie;
+
+  public Genero(String rock, String sertanejo, String pagode, String samba, String bossa_nova, String pop, String indie){
+    this.rock = rock;
+    this.sertanejo = sertanejo;
+    this.pagode = pagode;
+    this.samba = samba;
+    this.bossa_nova = bossa_nova;
+    this.pop = pop;
+    this.indie = indie;
+  }
+}
